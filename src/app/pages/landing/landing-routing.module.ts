327f31556a4dd355ae3e9300401df177af34e448
Taniaflsida <60895091+Taniaflsida@users.noreply.github.com>
2021-05-06 15:41:44 +0200

    git commit -m "DOOLE-657 - 00_00_02 condiciones"

--- src/app/pages/landing/landing-routing.module.ts
+++ src/app/pages/landing/landing-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: LandingPage
+  },
+  {
+    path: 'legal',
+    loadChildren: () => import('../legal/legal.module').then( m => m.LegalPageModule)
   }
 ];
 
