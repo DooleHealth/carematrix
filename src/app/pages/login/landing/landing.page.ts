2c4c925620a7b08cc0e1d15104ac9ce99f72c8f4
davidvalarezo <david.v.13@hotmail.es>
2021-05-10 14:10:30 +0200

    Add FormGroup
    Error to add FormGroup and FormControlName

--- src/app/pages/login/landing/landing.page.ts
+++ src/app/pages/login/landing/landing.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 
@@ -8,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.page.scss'],
 })
 export class LandingPage implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', 
+      Validators.compose([
+        Validators.required
+      ])),
+      password: new FormControl('',
+        Validators.compose([
+        Validators.required])
+      )
+    });
+   }
 
   ngOnInit() {
+  }
+
+  doDooleAppLogin(){
+    if (!this.loginForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log(this.loginForm.value)
+    }
   }
  
 
