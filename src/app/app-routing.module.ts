58650ba51140439cbd5beb8d101c1d8cd74cb5bd
Taniaflsida <60895091+Taniaflsida@users.noreply.github.com>
2021-05-19 18:34:21 +0200

    git commit -m "DOOLE-668 - 02_00 mis datos"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,6 +41,10 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'personal',
+    loadChildren: () => import('./pages/profile/personal/personal.module').then( m => m.PersonalPageModule)
   }
 
 ];
