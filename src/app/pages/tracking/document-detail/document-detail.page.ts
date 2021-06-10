fb31732c798df930d1882c06b566780811cf72bb
davidvalarezo <david.v.13@hotmail.es>
2021-06-10 09:23:25 +0200

    git commit -m "DOOLE-766 - 05_00 Documentos"

--- src/app/pages/tracking/document-detail/document-detail.page.ts
+++ src/app/pages/tracking/document-detail/document-detail.page.ts
@@ -1,30 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoadingController } from '@ionic/angular';
+import { DooleService } from 'src/app/services/doole.service';
+export interface ItemDiagnostic {
+  expanded?: boolean;
+  elements?: any[];
+}
 @Component({
   selector: 'app-document-detail',
   templateUrl: './document-detail.page.html',
   styleUrls: ['./document-detail.page.scss'],
 })
 export class DocumentDetailPage implements OnInit {
-  public items: any = [];
+  public items: ItemDiagnostic[] = [];
+  private id;
+  diagnosticTest = [];
+  diagnosticTestType = [];
 
+  groupedElements: any = [];
+  elementValues: any = [];
+  mediaFiles: any;
 
-  constructor() {
-    this.items = [
-      { expanded: false },
-      { expanded: false },
-      { expanded: false },
-      { expanded: false },
-      { expanded: false },
-      { expanded: false },
-      { expanded: false },
-      { expanded: false },
-      { expanded: false }
-    ];
+  constructor(
+    private loadingController: LoadingController,
+    private dooleService: DooleService
+  ) {
   }
 
 
   ngOnInit() {
+    this.id = history.state.id;
+    console.log('[DocumentDetailPage] ngOnInit()', this.id);
+    if(this.id)
+    this.getDiagnosticData()
   }
 
 
@@ -42,4 +49,115 @@ export class DocumentDetailPage implements OnInit {
       });
     }
   }
+
+  addItems(){
+    console.log('[DocumentDetailPage] expandItems()');
+    this.groupedElements.forEach(element => {
+      this.items.push({expanded: false, elements: element })
+    });
+  }
+
+  async getDiagnosticData(){
+    const loading = await this.loadingController.create();
+    await loading.present();
+    this.dooleService.getAPIdiagnosticTestID(this.id).subscribe(
+      async (res: any) =>{
+        console.log('[TrackingPage] getDiagnosticData()', await res);
+        this.diagnosticTest = res.diagnosticTest
+        this.diagnosticTestType = res.diagnosticTestType
+        if (res.diagnosticTest.media) {
+          this.mediaFiles = res.diagnosticTest.media;
+        }
+        if (res.elements) {
+          // Iterate elements in the tree searching for element groups
+          this.treeIterate(res.elements, '');
+          // Order data by group Name
+          this.groupedElements.sort(function (a, b) {
+            return a.group.localeCompare(b.group);
+          })
+          this.addItems()
+        }
+
+        loading.dismiss();
+       },(err) => { 
+        loading.dismiss();
+          console.log('[TrackingPage] getDiagnosticData() ERROR(' + err.code + '): ' + err.message); 
+          throw err; 
+      }) ,() => {
+        // Called when operation is complete (both success and error)
+        loading.dismiss();
+      };
+  }
+
+  treeIterate(obj, stack) {
+    for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+            if (typeof obj[property] == "object") {
+              this.treeIterate(obj[property], stack + '.' + property);
+            } else {
+                if(property=="group")
+                  this.groupedElements.push(obj);
+            }
+        }
+    }
+  }
+
+
+/*   async getData() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    this.authService.get('user/diagnosticTest/' + this.id).subscribe(
+      async (json) => {
+
+        await this.getTypes();
+
+        console.log('getData: ', await json);
+        console.log('this.id : ', this.id);
+
+        this.diagnosticTest = json.diagnosticTest;
+        this.diagnosticTestType = json.diagnosticTestType;
+
+        console.log("this.diagnosticTest.diagnostic_test_type_id ", this.diagnosticTestType.id);
+        if (json.diagnosticTest.media) {
+          this.mediaFiles = json.diagnosticTest.media;
+        }
+
+        // Iterate elements in the tree searching for element groups
+        this.treeIterate(json.elements, '');
+
+        // Order data by group Name
+        this.groupedElements.sort(function (a, b) {
+          return a.group.localeCompare(b.group);
+        })
+
+        this.elementValues = json.elementValues;
+
+        let values = {
+          type: this.diagnosticTestType.name,
+          title: this.diagnosticTest.title,
+          date: this.diagnosticTestType.created_at,
+          description: this.diagnosticTest.description,
+          images: [this.images]
+        }
+
+        //this.form.patchValue(values);
+
+      },
+      (error) => {
+        // Called when error
+        console.log("error: ", error);
+        throw new HttpErrorResponse(error);
+      },
+      () => {
+        // Called when operation is complete (both success and error)
+        loading.dismiss();
+      });
+  } */
+
+  openFile(media){
+    console.log("media", media);
+    window.open(media.temporaryUrl, "");
+  }
+
 }
--- src/app/pages/tracking/tracking.page.ts
+++ src/app/pages/tracking/tracking.page.ts
@@ -1,14 +1,74 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DooleService } from 'src/app/services/doole.service';
+export interface ListDiagnosticTests {
+  date?: string;
+  diagnosticTests?: any[];
+}
 @Component({
   selector: 'app-tracking',
   templateUrl: './tracking.page.html',
   styleUrls: ['./tracking.page.scss'],
 })
 export class TrackingPage implements OnInit {
-
-  constructor() { }
+  listDiagnostic: ListDiagnosticTests[] = []
+  diagnosticTests = []
+  forms = []
+  graphics = []
+  segment = 'documents'
+  constructor(
+    private dooleService: DooleService
+  ) { }
 
   ngOnInit() {
+    this.getDiagnosticTests()
+    //this.getForms()
   }
+
+  getDiagnosticTests(){
+    this.dooleService.getAPIdiagnosticTests().subscribe(
+      async (res: any) =>{
+        console.log('[TrackingPage] getDiagnosticTests()', await res);
+        this.diagnosticTests = res.diagnosticTests
+        this.filterDiagnosticsByDate()
+       },(err) => { 
+          console.log('[TrackingPage] getDiagnosticTests() ERROR(' + err.code + '): ' + err.message); 
+          throw err; 
+      });
+  }
+
+  getForms(){
+    this.dooleService.getAPIformsTracking().subscribe(
+      async (res: any) =>{
+        console.log('[TrackingPage] getForms()', await res);
+        this.forms = res
+       },(err) => { 
+          console.log('[TrackingPage] getForms() ERROR(' + err.code + '): ' + err.message); 
+          throw err; 
+      });
+  }
+
+  getGraphics(){
+    this.dooleService.getAPIformsTracking().subscribe(
+      async (res: any) =>{
+        console.log('[TrackingPage] getGraphics()', await res); 
+        this.graphics = res
+       },(err) => { 
+          console.log('[TrackingPage] getGraphics() ERROR(' + err.code + '): ' + err.message); 
+          throw err; 
+      });
+  }
+
+  filterDiagnosticsByDate(){
+    this.diagnosticTests.forEach( (diagnostic, index) =>{
+      let date = diagnostic.date_european
+      if(index == 0 || date !== this.diagnosticTests[index-1].date_european){
+        let list = this.diagnosticTests.filter( event => 
+          (event.date_european == date)
+        )
+        this.listDiagnostic.push({date: diagnostic.data, diagnosticTests: list}) 
+      } 
+    })
+    console.log('[TrackingPage] filterDiagnosticsByDate()', this.listDiagnostic);
+  }
+
 }
