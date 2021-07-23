50b5077af07db3c248fc29b10ae2206d56e4d861
Taniaflsida <60895091+Taniaflsida@users.noreply.github.com>
2021-07-23 17:45:12 +0200

    CHANGE google services and enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,12 +9,13 @@ export const environment = {
     networkDelay: 500
   },
   firebase : {
-    apiKey: "AIzaSyDcd9Q5boJJ46VGoGn1MYAN3BOJp6EaCv8",
-    authDomain: "covid-39b96.firebaseapp.com",
-    databaseURL: "https://covid-39b96.firebaseio.com/",
-    projectId: "covid-39b96",
-    storageBucket: "covid-39b96.appspot.com",
-    messagingSenderId: "344383195320",
+    apiKey: "AIzaSyDqlLbR0uBI7JM6XgCxL0vbLO8Mv1zfFrc",
+    authDomain: "deneb-65a05.firebaseapp.com",
+    databaseURL: "https://deneb-65a05-default-rtdb.firebaseio.com",
+    projectId: "deneb-65a05",
+    storageBucket: "deneb-65a05.appspot.com",
+    messagingSenderId: "429887824166",
+    appId: "1:429887824166:web:8ae6abb69c876697d121e7"
     //appId: "1:1094231160196:web:11d4eecd087e565cbed9d1",
     //measurementId: "G-4SW50F4HMH"
 
