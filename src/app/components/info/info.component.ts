d486dc0121aae0847eaabb8277fb2a3093ad61c8
Taniaflsida <60895091+Taniaflsida@users.noreply.github.com>
2021-07-30 11:05:07 +0200

    git commit -m "DOOLE-1206 - COMPONENTE icono de información"

---
+++ src/app/components/info/info.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
+
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.scss'],
+})
+export class InfoComponent implements OnInit {
+
+  // @Input('title') title: string;
+  // @Input('icon') icon: string;
+  @Input('info') info: string;
+  // info: string;
+
+  constructor(public translate: TranslateService, public alertController: AlertController) { }
+
+
+  ngOnInit(
+
+    ) {}
+  
+    async presentAlert() {
+      
+      this.translate.get('info.button').subscribe(
+        async button => {
+          // value is our translated string
+          const alert = await this.alertController.create({
+            cssClass: "alertClass",
+            header: this.translate.instant('info.title'),
+            // subHeader: 'Subtitle',
+            message: this.info,
+            buttons: [button]
+          });
+      
+          await alert.present();
+        });
+      
+    }
+  }
+  
