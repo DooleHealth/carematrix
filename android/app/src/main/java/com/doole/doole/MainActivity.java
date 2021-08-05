34eb0fdafc8ff94d60998fcef61d98e251f701f3
davidvalarezo <david.v.13@hotmail.es>
2021-08-05 13:55:40 +0200

    changes save

--- android/app/src/main/java/com/doole/doole/MainActivity.java
+++ android/app/src/main/java/com/doole/doole/MainActivity.java
@@ -1,10 +1,10 @@
-package com.doole.app;
+package com.doole.doole;
 
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
-import ch.byrds.capacitor.contacts.Contacts;
+
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
@@ -16,7 +16,6 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
-      add(Contacts.class);
     }});
   }
 }
