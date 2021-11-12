9101d3128e3d3e6120c5e7b32e69ec8e0bf97779
davidvalarezo <david.v.13@hotmail.es>
2021-11-12 12:48:47 +0100

    git commit -m "DOOLE-2218 - Error al cambiar el idioma cuando en el centro solo hay un idioma"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -53,7 +53,7 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule),
-    canLoad: [AuthGuard] // Secure all child pages
+    //canLoad: [AuthGuard] // Secure all child pages
   },
   {
     path: 'personal',
