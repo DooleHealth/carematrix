8f9aefc266c71430e609d7b340421fcca6284f50
davidvalarezo <david.v.13@hotmail.es>
2022-05-02 15:43:20 +0200

    DOOLE-2907

--- src/app/services/pusher/pusher-notification.service.ts
+++ src/app/services/pusher/pusher-notification.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Constants } from '../config/constants';
-import { AuthenticationService } from './authentication.service';
-import { NotificationService } from './notification.service';
+import { Constants } from 'src/app/config/constants';
+import { AuthenticationService } from '../authentication.service';
+import { NotificationService } from '../notification.service';
 declare const Pusher: any;
 const NAME_BIND = 'App\\Events\\LevelAccomplishmentCompleted' 
+
 @Injectable({
   providedIn: 'root'
 })
-export class PusherService {
+export class PusherNotificationService {
   app_id = "1287334"
   key = "f89e2ed013b43522069e"
   secret = "e23e0b09095890bdac73"
@@ -41,3 +42,4 @@ export class PusherService {
         });
   }
 }
+
