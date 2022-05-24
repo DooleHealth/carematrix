5f4102c30b0e53a9d86ef7ff52726eb78f2b3532
davidvalarezo <david.v.13@hotmail.es>
2022-05-24 12:09:38 +0200

    cambio dinamico en pusher mensajería

--- src/app/services/pusher/pusher-message.service.ts
+++ src/app/services/pusher/pusher-message.service.ts
@@ -11,6 +11,23 @@ const NAME_BIND = 'App\\Events\\MessageCreated'
   providedIn: 'root'
 })
 export class PusherMessageService {
+  public  LIST_APP_KEY: Array<any> = [
+    {
+      name: 'Producción', 
+      app_id: "1287334",
+      key: "f89e2ed013b43522069e",
+      secret: "e23e0b09095890bdac73",
+      cluster: "eu"
+    },
+    {
+      name: 'Calidad', 
+      app_id: "1287332",
+      key: "d560960f0cc446a18c95",
+      secret: "63522acb9ee1832855a4",
+      cluster: "eu"
+    }
+  ]
+
   app_id = "1287334"
   key = "f89e2ed013b43522069e"
   secret = "e23e0b09095890bdac73"
@@ -22,6 +39,7 @@ export class PusherMessageService {
     private constants: Constants,
     private authService: AuthenticationService
   ) {
+    this.setEndPoint()
     const TOKEN = this.authService.getAuthToken() 
     this.pusher = new Pusher(this.key, {
       cluster: this.cluster,
@@ -41,5 +59,15 @@ export class PusherMessageService {
     console.log('[PusherMessageService] init()', this.pusher, this.nameChanel);
     return this.channel;
  }
+
+ public setEndPoint(){
+  let index = this.constants?.INDEX
+  let opt = this.LIST_APP_KEY[index]
+  console.log('[AppComponent] setEndPoint() ' ,  opt);
+  this.app_id = opt.app_id
+  this.key = opt.key
+  this.secret = opt.secret
+  this.cluster = opt.cluster
+}
 
 }
