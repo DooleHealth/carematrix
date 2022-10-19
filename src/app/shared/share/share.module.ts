5d9f379a0537cf939eb13169f686dcce1ffbab52
isaacmiliani <isaacmiliani@gmail.com>
2022-10-19 22:30:03 +0200

    added cached images for homepage

--- src/app/shared/share/share.module.ts
+++ src/app/shared/share/share.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeHtmlPipe } from '../pipes/safe-html.pipe';
+import { CachedImageComponent } from 'src/app/components/cached-image/cached-image.component';
+import { IonicModule } from '@ionic/angular';
+
 
 
 
 @NgModule({
   declarations: [SafeHtmlPipe],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   exports: [SafeHtmlPipe]
 })
