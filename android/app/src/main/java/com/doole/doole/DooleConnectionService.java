ba4d5f5cd12c323efd70220843673ad610966361
isaacmiliani <isaacmiliani@gmail.com>
2023-07-19 13:26:21 +0200

    save

--- android/app/src/main/java/com/doole/doole/DooleConnectionService.java
+++ android/app/src/main/java/com/doole/doole/DooleConnectionService.java
@@ -14,6 +14,7 @@ public class DooleConnectionService extends ConnectionService {
     public String app_name = "Doole";
     private static String TAG = "DooleConnectionService";
     private static Connection conn;
+
     Connection mConnection;
     public static Connection getConnection() {
         return conn;
