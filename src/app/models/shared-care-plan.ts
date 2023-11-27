435d8104c46520d3360f8d6701952db8d211623d
davidvalarezo <david.v.13@hotmail.es>
2023-11-27 16:04:47 +0100

    save changes

--- src/app/models/shared-care-plan.ts
+++ src/app/models/shared-care-plan.ts
@@ -4,6 +4,7 @@ export interface SharedCarePlan {
     description: string;
     type: string;
     state?: string;
+    route?: string;
 }
 
 export interface SharedCarePlanGoal extends SharedCarePlan  {
@@ -11,3 +12,31 @@ export interface SharedCarePlanGoal extends SharedCarePlan  {
     date: string;
     percentage: string
 }
+
+enum ContentTypePath {
+    Goals,
+    LifestyleHabits,
+    Medication,
+    Forms,
+    Monitoring,
+    MedicalProcedure,
+    InformedConsent,
+}
+
+enum ContentTypeIcons {
+    Goals = 'assets/icons/share-care-plan/scp-goals.svg',
+    LifestyleHabits = 'assets/icons/share-care-plan/scp-lifestyle.svg',
+    Medication = 'assets/icons/share-care-plan/scp-medication.svg',
+    Forms = 'assets/icons/share-care-plan/scp-form.svg',
+    Monitoring = 'assets/icons/share-care-plan/scp-monitoring.svg',
+    MedicalProcedure = 'assets/icons/share-care-plan/scp-medical-procedures.svg',
+    InformedConsent = 'assets/icons/share-care-plan/scp-informed.svg',
+}
+
+interface SCPContentType {
+    [id: number]: SharedCarePlan
+}
+
+const ListContentType: SCPContentType = [
+
+]
