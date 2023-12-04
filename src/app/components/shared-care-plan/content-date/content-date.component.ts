396b3b298b1b02c7c54c137732849f06db76cbea
davidvalarezo <david.v.13@hotmail.es>
2023-12-04 17:23:15 +0100

    save changes

--- src/app/components/shared-care-plan/content-date/content-date.component.ts
+++ src/app/components/shared-care-plan/content-date/content-date.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { LifestyleIndexComponent } from '../lifestyle-index/lifestyle-index.component';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { SharedCarePlanProcedure } from 'src/app/models/shared-care-plan';
 
 @Component({
   selector: 'app-content-date',
   templateUrl: './content-date.component.html',
   styleUrls: ['./content-date.component.scss'],
 })
-export class ContentDateComponent extends LifestyleIndexComponent implements OnInit {
-
- // export class ContentComponent  implements OnInit {
-    // @Input() content: SharedCarePlan
-    // @Output() redirect: EventEmitter<any> = new EventEmitter<any>();
+export class ContentDateComponent implements OnInit {
+    @Input() content: SharedCarePlanProcedure
+    @Output() redirect: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() {
-    super();
+
   }
 
   ngOnInit() {}
+
+  goTo(type){
+    this.redirect.emit({type: type})
+  }
 
 }
