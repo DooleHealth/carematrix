64b9313a6fa24b60195a86389ba9e1187f4d04f7
RAbreuPatterson <146714853+RAbreuPatterson@users.noreply.github.com>
2024-04-12 15:22:25 +0200

    update

---
+++ src/app/directive/changeColorCargiver/change-color-cargiver.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ChangeColorCargiverDirective } from './change-color-cargiver.directive';
+
+describe('ChangeColorCargiverDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ChangeColorCargiverDirective();
+    expect(directive).toBeTruthy();
+  });
+});
