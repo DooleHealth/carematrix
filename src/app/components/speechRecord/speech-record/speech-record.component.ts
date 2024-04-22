5cefc84474516f2ad5d2d68ebea10cb012886ff7
joancahue <63915250+joancahue@users.noreply.github.com>
2024-04-22 11:47:19 +0200

    refactor styles on searchbar audio icon

--- src/app/components/speechRecord/speech-record/speech-record.component.ts
+++ src/app/components/speechRecord/speech-record/speech-record.component.ts
@@ -8,8 +8,8 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './speech-record.component.html',
   styleUrls: ['./speech-record.component.scss'],
 })
-export class SpeechRecordComponent  implements OnInit {
-  recoding= false;
+export class SpeechRecordComponent implements OnInit {
+  recoding = false;
   myText = "";
   @Input() type: any
   @Output() record: EventEmitter<any> = new EventEmitter<any>();
@@ -21,95 +21,84 @@ export class SpeechRecordComponent  implements OnInit {
     public translate: TranslateService,
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  async starRecognition(){
- // this.showAlertRecord()
-    const {available} = await SpeechRecognition.available();
-  
-    if(available){
-      this.recoding= true; 
+  async starRecognition() {
+    // this.showAlertRecord()
+    const { available } = await SpeechRecognition.available();
+
+    if (available) {
+      this.recoding = true;
       this.showAlertRecord();
       SpeechRecognition.start({
         popup: false,
         partialResults: true,
         language: 'es-ES',
-  
-  
-      });
-      SpeechRecognition.addListener('partialResults', (data: any)=>{
-        console.log("partialResults was fired", data.matches);
-       
-        if (this.platform.is('ios')) {
-        if(data.matches && data.matches.length > 0){
-          this.myText = data.matches[0];
-          this.record.emit(this.myText);
-          this.changeDetectorRef.detectChanges();
-        }
 
-        }
-        if (this.platform.is('ios')) {
-        //android
-        if(data.value && data.value.length > 0){
-          this.myText = data.value[0];
-          this.changeDetectorRef.detectChanges();
-        }  
-      }
+
+      });
+      SpeechRecognition.addListener('partialResults', (data: any) => {
+        console.log("partialResults was fired", data.matches);
+          if (data.matches && data.matches.length > 0) {
+            this.myText = data.matches[0];
+            this.record.emit(this.myText);
+            this.changeDetectorRef.detectChanges();
+          }
       })
     }
-  
+
   }
-  
-  async stopRecognition(){
+
+  async stopRecognition() {
     this.recoding = false;
     await SpeechRecognition.stop();
   }
 
-  async filterList(event){
-    
+  async filterList(event) {
+
     this.record.emit(event.srcElement.value);
   }
 
-  async getList(){
-    
+  async getList() {
+
     this.loadList.emit();
   }
 
- async showAlertRecord(){
-  const alert = await this.alertController.create({
-    cssClass: 'alertRecord',
-    header: this.translate.instant('alert.recording'),
-    message: `
+  async showAlertRecord() {
+    const alert = await this.alertController.create({
+      cssClass: 'alertRecord',
+      header: this.translate.instant('alert.recording'),
+      message: `
         <div class="alert-content">
           <img src="/assets/images/mic.gif" alt="GIF" id="micImage" style="max-width: 100%;">
         </div>
       `,
-  })
+    })
 
- 
 
-   
-   alert.onDidDismiss().then(() => {
-    // Aquí puedes realizar la operación que desees
-    console.log('El usuario cerró la alerta. Se puede hacer la operación aquí.');
-    // Por ejemplo, puedes llamar a una función que realice la operación
-    this.stopRecognition();
-  });
 
-  await alert.present();
-debugger
-  // Obtener el elemento del GIF dentro del alerta
-  const micImage = document.getElementById('micImage');
-  if (micImage) {
-    // Adjuntar evento click al elemento del GIF
-    micImage.addEventListener('click', () => {
-      console.log('El usuario hizo clic en el GIF dentro del alerta.');
-      // Aquí puedes realizar la operación que desees cuando el usuario hace clic en el GIF
+
+    alert.onDidDismiss().then(() => {
+      // Aquí puedes realizar la operación que desees
+      console.log('El usuario cerró la alerta. Se puede hacer la operación aquí.');
+      // Por ejemplo, puedes llamar a una función que realice la operación
       this.stopRecognition();
-      // También puedes cerrar el alerta después de hacer clic en el GIF si lo deseas
-      alert.dismiss();
     });
+
+    await alert.present();
+    debugger
+    // Obtener el elemento del GIF dentro del alerta
+    const micImage = document.getElementById('micImage');
+    if (micImage) {
+      // Adjuntar evento click al elemento del GIF
+      micImage.addEventListener('click', () => {
+        console.log('El usuario hizo clic en el GIF dentro del alerta.');
+        // Aquí puedes realizar la operación que desees cuando el usuario hace clic en el GIF
+        this.stopRecognition();
+        // También puedes cerrar el alerta después de hacer clic en el GIF si lo deseas
+        alert.dismiss();
+      });
+    }
   }
-}
 
 }
