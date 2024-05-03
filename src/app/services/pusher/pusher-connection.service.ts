8ee64f4c8c5cb7f989c1a2aa523af2a97bcf5649
gerardh11 <ghorta@doolehealth.com>
2024-05-03 14:43:17 +0200

    fix order

--- src/app/services/pusher/pusher-connection.service.ts
+++ src/app/services/pusher/pusher-connection.service.ts
@@ -9,6 +9,14 @@ declare const Pusher: any;
 })
 export class PusherConnectionService {
   public  LIST_APP_KEY: Array<any> = [
+   
+    {
+      name: 'Inca Health Producción',
+      app_id: "1753153",
+      key: "ab568085e820d78c40dd",
+      secret: "5d91abad2e285e24b38b",
+      cluster: "eu"
+    },
     {
       name: 'Inca Health Desarrollo',
       app_id: "1753151",
@@ -16,13 +24,6 @@ export class PusherConnectionService {
       secret: "7593af5392913fbc8c54",
       cluster: "eu"
       
-    },
-    {
-      name: 'Inca Health Producción',
-      app_id: "1753153",
-      key: "ab568085e820d78c40dd",
-      secret: "5d91abad2e285e24b38b",
-      cluster: "eu"
     }
   ]
   pusher
